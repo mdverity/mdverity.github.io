09279493617d165277bc1ac72ebdd2cab81e6cd2
Matt Verity <mdverity@gmail.com>
2021-03-10 14:26:36 -0500

    Added media queries and menu overlay responsiveness

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,20 +12,20 @@ menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     if(!showMenu) {
-        menuBtn.classList.add('close')
-        menu.classList.add('show')
-        menuNav.classList.add('show')
-        menuBranding.classList.add('show')
-        navItems.forEach(item => item.classList.add('show'))
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
 
         // Set menu state
         showMenu = true;
     } else {
-        menuBtn.classList.remove('close')
-        menu.classList.remove('show')
-        menuNav.classList.remove('show')
-        menuBranding.classList.remove('show')
-        navItems.forEach(item => item.classList.remove('show'))
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
 
         // Set menu state
         showMenu = false;
